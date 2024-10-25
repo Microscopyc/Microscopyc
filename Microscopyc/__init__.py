e78716750405a87a2053804995ae5225c7ee14e1
Carolina Salinas D <carolina.salinas@mi.unc.edu.ar>
2024-10-24 23:53:59 -0300

    organize&new_files

---
+++ Microscopyc/__init__.py
@@ -0,0 +1,11 @@
+# MIT License
+# Copyright (c) 2024, Salinas Domjan, Carolina; Brizuela Horacio J.
+# All rights reserved.
+
+"""Microscopyc is a tool to identify and analyze microscopic images,
+particularly biological cells.
+It contains two modules, one for training, in case users want to add
+new categories to identify, and the second module to categorize the user's
+images based on the cell type and process the images with an output that
+compare size, groupings, morphological differences and cells per pixel for
+each category."""
